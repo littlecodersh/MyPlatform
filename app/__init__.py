4573a3ab00ed0076bece4b2544f5f0e1a00c31e2
LittleCoder <i7meavnktqegm1b@qq.com>
2016-05-11 07:29:58 +0800

    Fix import error when flask not installed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
+try:
+    from flask import Flask
+    app = Flask(__name__)
+    import views
+    # init_menu()
+    # from app.plugins.menu import init_menu
+except:
+    pass
 
-app = Flask(__name__)
 
-import views
-
-# from app.plugins.menu import init_menu
-# init_menu()
